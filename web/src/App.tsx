9c343d8ba7c9f771efbc4b8179395efa4eca63d0
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-03 14:07:55 -0300

    Create Routes Structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-import Landing from './pages/Landing';
+import Routes from './routes';
 
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Routes />
   );
 }
 
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TeacherList() {
+  return (
+    <h1>Teachers</h1>
+  );
+}
+
+export default TeacherList;
