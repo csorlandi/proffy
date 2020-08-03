f53e88b3fe61d36f5809dbb5513993fc7b4294ab
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-03 12:18:42 -0300

    Finishing Landing Page Layout

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -27,8 +27,8 @@ function Landing() {
           </a>
 
           <a href="" className="give-classes">
-            <img src={giveClassesIcon} alt="Estudar" />
-            Estudar
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
           </a>
         </div>
 
