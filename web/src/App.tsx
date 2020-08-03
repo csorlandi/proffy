7c8a0f339712a169a8db1b777a7ad4a8c6afd058
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-03 11:27:11 -0300

    Creating Home Page as Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Landing />
     </div>
   );
 }
