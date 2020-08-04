85a30ca2d8ea96d5844a05222a685b10fb12031d
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-04 01:42:13 -0300

    Finishing Create Connection Feature

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -74,6 +74,8 @@ export default class ClassesController {
 
       return response.status(201).send();
     } catch (err) {
+      console.log(err);
+
       await trx.rollback();
 
       return response.status(400).json({
