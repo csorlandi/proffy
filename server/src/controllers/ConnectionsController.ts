263ba85c204474916194c0f40bd9f2062325161e
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-04 01:47:42 -0300

    Finishing List Total Connections Feature

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -2,7 +2,13 @@ import { Request, Response } from 'express';
 import db from '../database/connection';
 
 export default class ConnectionsController {
-  async index (request: Request, response: Response) {}
+  async index (request: Request, response: Response) {
+    const totalConnections = await db('connections').count('* as total');
+
+    const { total } = totalConnections[0];
+
+    return response.json({ total });
+  }
 
   async create(request: Request, response: Response) {
     const { user_id } = request.body;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,6 +10,7 @@ const connectionsController = new ConnectionsController();
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
+routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
 
 export default routes;
