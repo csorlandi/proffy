ab0ce9a00d0551171bf4c3bc694651182aa87d08
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-04 01:06:45 -0300

    Finishing Create Class Feature

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+app.use(routes);
 
 app.listen(3333);
