4332476e099f42c196806dcade88f42c3a590e7e
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-04 01:32:08 -0300

    Finishing List Classes Feature

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import ClassesController from './controllers/ClassesController';
 const routes = express.Router();
 const classesController = new ClassesController();
 
+routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
 export default routes;
