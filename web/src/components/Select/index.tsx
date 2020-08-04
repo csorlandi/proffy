91ba55f0a978a81948c2ea6c4ffc905b2d791e5c
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-04 19:21:13 -0300

    Finish Teacher Register connecting Backend and Frontend

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -15,7 +15,7 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select id={name} defaultValue="" {...rest}>
+      <select id={name} value="" {...rest}>
         <option value="" disabled hidden>Selecione uma opção</option>
         {options.map(option => {
           return <option key={option.value} value={option.value}>{option.label}</option>;
