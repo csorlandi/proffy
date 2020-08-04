06771078132a944a2028c2a208957539b0759d75
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-03 23:52:49 -0300

    Testing Request Body

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.post('/users', (request, response) => {
+  console.log(request.body);
+  
   const users = [
     { name: 'Diego', age: 25 },
     { name: 'Vini', age: 21 },
