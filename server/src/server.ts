81729654a49d0d8e87b20b7c39391eb098e4cf82
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-03 23:32:12 -0300

    Change Route to return a JSON to Client

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,13 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', () => {
-  console.log('Acessou a rota!');
+app.get('/users', (request, response) => {
+  const users = [
+    { name: 'Diego', age: 25 },
+    { name: 'Vini', age: 21 },
+  ];
+
+  return response.json(users);
 });
 
 app.listen(3333);
