047d7839eb35675a3b00208ef6ad55a3f6ef9fac
Claudio Orlandi <cso.junior1996@gmail.com>
2020-08-05 22:16:45 -0300

    Create Page Header Component

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 function Favorites() {
-  return <View style={styles.container} />
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys disponíveis" />
+    </View>
+  );
 }
 
 export default Favorites;
